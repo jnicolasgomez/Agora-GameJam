c48aaf2153781efdf0721c8caf9de7aaf75ef956
Nixperful <nicolas.osorio@mail.escuelaing.edu.co>
2018-07-07 15:59:24 -0500

    Integracion con personaje y camaras

---
+++ Assets/Character/Script/cameraCollision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraCollision : MonoBehaviour {
+
+
+	public float minDistance=1.0F;
+	public float maxDistance= 4.0F;
+	public float smooth=10.0F;
+	Vector3 dollyDir;
+	public Vector3 dollyDirAdjusted;
+	public float distance;
+	
+	// Use this for initialization
+	void Awake() {
+		dollyDir = transform.localPosition.normalized;
+		distance = transform.localPosition.magnitude;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir* maxDistance);
+		RaycastHit hit;
+		
+		if(Physics.Linecast(transform.parent.position, desiredCameraPos, out hit)){
+			distance= Mathf.Clamp((hit.distance*0.8F),minDistance,maxDistance);
+		}
+		else{
+			distance=maxDistance;
+		}
+		transform.localPosition = Vector3.Lerp(transform.localPosition,dollyDir*distance, Time.deltaTime*smooth);
+	}
+}
---
+++ Assets/Character/Script/cameraFollow.cs
@@ -0,0 +1,68 @@
+﻿			using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+	public float CameraMoveSpeed = 120.0f;
+	public GameObject CameraFollowObj;
+	public float clampAngle = 80.0f;
+	public float mouseSensitivity= 150.0f;
+	public GameObject CameraObj;
+	public GameObject PlayerObj;
+	public float camDistanceXToPlayer;
+	public float camDistanceYToPlayer;
+	public float camDistanceZToPlayer;
+	public float mouseX;
+	public float mouseY;
+	public float finalInputX;
+	public float finalInputZ;
+	public float smoothX;
+	public float smoothY;
+	public float rotX= 0.0f;
+	public float rotY= 0.0f;
+	Vector3 followPOS;
+	
+	
+	
+
+	// Use this for initialization
+	void Start () {
+		Vector3 rot = transform.localRotation.eulerAngles;
+		rotX=rot.x;
+		rotY=rot.y;
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		float inputX = Input.GetAxis("RightStickHorizontal");
+		float inputZ= Input.GetAxis("RightStickVertical");
+		mouseX = Input.GetAxis("Mouse X");
+		mouseY = Input.GetAxis("Mouse Y");
+		finalInputX = inputX + mouseX;
+		finalInputZ = inputZ + mouseY;
+		
+		
+		rotY += finalInputX * mouseSensitivity * Time.deltaTime;
+		rotX += finalInputZ * mouseSensitivity * Time.deltaTime;
+		
+		rotX = Mathf.Clamp(rotX,-clampAngle,clampAngle);
+		Quaternion localRotation= Quaternion.Euler(rotX,rotY,0.0f);
+		transform.rotation=localRotation;
+	}
+	
+	void LateUpdate(){
+		CameraUpdater();
+	}
+	void CameraUpdater(){
+		Transform target = CameraFollowObj.transform;
+		
+		float step = CameraMoveSpeed *Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+	}
+}
