74a7d9b6be670429291076929c66acd1e7e0a625
JuanNicolasGomez <nicolas.gm243@gmail.com>
2018-07-07 08:37:38 -0500

    first commit obstacles

---
+++ Assets/Script/MovingPlatform.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    public Transform movingPlat;
+    public Transform position1;
+    public Transform position2;
+    public Vector3 newPosition;
+
+    public string currentState;
+    public float smooth;
+    public float resetTime;
+	// Use this for initialization
+	void Start () {
+        //newPosition = position1.position;
+        ChangeTarget();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        movingPlat.position = Vector3.Lerp(movingPlat.position, newPosition, smooth * Time.deltaTime);
+        
+        /*if (movingPlat.position.x - newPosition.x < 0.5) {
+            Debug.Log("entraaaaaa");
+            
+            ChangeTarget();
+        }*/
+        //ChangeTarget();
+	}
+
+    void ChangeTarget() {
+        if (currentState == "Moving To Position 1")
+        {
+            currentState = "Moving To Position 2";
+            newPosition = position2.position;
+        }
+        else if (currentState == "Moving To Position 2")
+        {
+            currentState = "Moving To Position 1";
+            newPosition = position1.position;
+        }
+        else if (currentState == "") {
+            currentState = "Moving To Position 2";
+            newPosition = position2.position;
+        }
+        Invoke("ChangeTarget", resetTime);
+    }
+}
---
+++ Assets/Script/RotationWheel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationWheel : MonoBehaviour {
+
+    public float speed = 50.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.right * speed * Time.deltaTime);
+	}
+}
